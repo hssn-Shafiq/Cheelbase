92c4af56b23fc2c2d9884830ca32b01048de627f
Hassan Shafiq <shafiqh850@gmail.com>
2024-01-04 17:42:03 +0500

    main.js/images-path/updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,79 +2,79 @@ const allStories = [
   {
     id: 0,
     author: "Jiane",
-    imageUrl: "assets/images/3.jpg",
+    imageUrl: "/assets/images/3.jpg",
   },
 
   {
     id: 1,
     author: "Willow Grace",
-    imageUrl: "assets/images/2.jpg",
+    imageUrl: "/assets/images/2.jpg",
   },
 
   {
     id: 2,
     author: "Emma Smith",
-    imageUrl: "assets/images/3.jpg",
+    imageUrl: "/assets/images/3.jpg",
   },
 
   {
     id: 3,
     author: "Ruby Skye",
-    imageUrl: "assets/images/4.jpg",
+    imageUrl: "/assets/images/4.jpg",
   },
 
   {
     id: 4,
     author: "Live Blogger",
-    imageUrl: "assets/images/5.jpg",
+    imageUrl: "/assets/images/5.jpg",
   },
 
   {
     id: 5,
     author: "Hazel Jade",
-    imageUrl: "assets/images/6.jpg",
+    imageUrl: "/assets/images/6.jpg",
   },
 
   {
     id: 6,
     author: "Eden Faith",
-    imageUrl: "assets/images/7.jpg",
+    imageUrl: "/assets/images/7.jpg",
   },
 
   {
     id: 7,
     author: "Flora Maeve",
-    imageUrl: "assets/images/8.jpg",
+    imageUrl: "/assets/images/8.jpg",
   },
 
   {
     id: 8,
     author: "Nathaniel Hayes",
-    imageUrl: "assets/images/9.jpg",
+    imageUrl: "/assets/images/9.jpg",
   },
 
   {
     id: 9,
     author: "Everett Lee",
-    imageUrl: "assets/images/10.jpg",
+    imageUrl: "/assets/images/10.jpg",
   },
 
   {
     id: 9,
     author: "Owen",
-    imageUrl: "assets/images/11.jpg",
+    imageUrl: "/assets/images/11.jpg",
   },
 
   {
     id: 9,
     author: "Caleb Knox",
-    imageUrl: "assets/images/12.jpg",
+    imageUrl: "/assets/images/12.jpg",
   },
 
   {
     id: 9,
     author: "Felix",
-    imageUrl: "assets/images/13.jpg",
+    imageUrl: "/assets/images/13.jpg",
   },
 ];
 
